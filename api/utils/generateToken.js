ddc760d5d0c4e674ff02380fac3d042672b01305
AtharvShelke <shelkeatharv964@gmail.com>
2024-09-25 19:07:46 +0530

    changes in generateToken sameSite:'strict' and secure:'production'

--- api/utils/generateToken.js
+++ api/utils/generateToken.js
@@ -2,14 +2,15 @@ import jwt from 'jsonwebtoken';
 
 const secret = 'asdfghjkl'
 
-const generateToken = (res, username)=>{
-    const token = jwt.sign({username}, secret, {
-        expiresIn:'1d'
+const generateToken = (res, username) => {
+    const token = jwt.sign({ username }, secret, {
+        expiresIn: '1d'
     });
     res.cookie('token', token, {
-        httpOnly:true,
-        
-        maxAge: 1000 * 60 * 60 *24
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', // only secure in production
+        sameSite: 'strict',
+        maxAge: 1000 * 60 * 60 * 24
     })
     console.log('Token Generated:', token);
 }
@@ -24,4 +25,4 @@ const deleteToken = (res) => {
     console.log('Token Destroyed');
 }
 
-export  {generateToken, deleteToken};
+export { generateToken, deleteToken };
